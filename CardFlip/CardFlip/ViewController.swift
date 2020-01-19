d070f97c40ebc6a0e64e479a3a3e376d9bfaf980
mkaulfers <matthewkaulfers@Matthews-MacBook-Pro.local>
2020-01-18 21:52:50 -0600

    Feature: Added card flip animations.

--- CardFlip/CardFlip/ViewController.swift
+++ CardFlip/CardFlip/ViewController.swift
@@ -12,86 +12,144 @@ class ViewController: UIViewController {
     
     //MARK: - Variables
     //INFO: Cards
-
+    @IBOutlet var cardsPhone: [UIImageView]!
     
     //INFO: Other UI Variables
     @IBOutlet weak var playButtonView: UIImageView!
     @IBOutlet weak var cardTypeSelection: UISegmentedControl!
     
     //INFO: Static Variables
-    var cards: [UIImageView] = [UIImageView]()
+    var cardsImages: [UIImage] = [UIImage]()
+    
+    //INFO: Image Set Variable
+    var imageSet = "r"
     
     //MARK: - Start of Main
     override func viewDidLoad() {
         super.viewDidLoad()
-        setCardView()
-    }
-    
-    override func viewWillAppear(_ animated: Bool) {
+        cardsPhone.sort(by: {$0.tag < $1.tag})
         setCardView()
     }
     
     //MARK: - Setup Methods
-    
     func setCardView()
     {
-        var randomSelection = [Int.random(in: 0...1), Int.random(in: 2...3),Int.random(in: 4...5),Int.random(in: 6...7),Int.random(in: 8...9),Int.random(in: 10...11),Int.random(in: 12...13),Int.random(in: 14...15),Int.random(in: 16...17),Int.random(in: 18...19)]
-        
-        randomSelection.shuffle()
-        
-        for i in 0...9
+        //INFO: Add GestureRecognizer to each card
+        for card in cardsPhone
         {
-            cards[i].image = UIImage(named: "r\(randomSelection[i])")
-            cards[i+10].image = UIImage(named: "r\(randomSelection[i])")
+            card.addGestureRecognizer(UITapGestureRecognizer.init(target: self, action: #selector(playCard(_:))))
+        }
+        
+        //INFO: Create a set, they cannot contain duplicates.
+        var selectedImages = Set<UIImage>()
+        
+        //INFO: Loop through adding random images until we reach a count of 10
+        while selectedImages.count < 10
+        {
+            selectedImages.insert(UIImage(named: "\(imageSet)\(Int.random(in: 0...19))")!)
+        }
+        
+        //INFO: Convert set to an array, then shuffle.
+        cardsImages = Array(selectedImages)
+        cardsImages += cardsImages
+        cardsImages.shuffle()
+        
+        //INFO: Assign images from our selectedImagesArray to the cards.
+        for (i, card) in cardsPhone.enumerated()
+        {
+            card.image = cardsImages[i]
         }
     }
     
     //MARK: - Custom Methods
     @IBAction func playButtonPressed()
     {
+        DispatchQueue.global(qos: .default).async {
+            for card in self.cardsPhone
+            {
+                DispatchQueue.main.async {
+                    self.flipCard(sender: card)
+                }
+                usleep(20000)
+            }
+        }
+        cardsSelected = 0
+        firstSelectedCard = UIImage()
+        secondSelectedCard = UIImage()
         setCardView()
-        
-//        UIImageView.animate(withDuration: 0.5,  animations: {
-//            self.playButtonView.center = CGPoint(x: self.playButtonView.center.x, y: self.playButtonView.center.y + 15)
-//
-//        })
     }
-    
-    //MARK: - GestureRecognizer Actions
-    
     
     //MARK: - Play Logic
-    var firstSelectedCard = UIImageView()
-    var secondSelectedCard = UIImageView()
+    var firstSelectedCard = UIImage()
+    var secondSelectedCard = UIImage()
     var cardsSelected = 0
     
-    func playCard(card: UIImageView)
+    @objc func playCard(_ sender: UITapGestureRecognizer)
     {
-        cardsSelected += 1
         
-        switch cardsSelected
-        {
-        case 1:
-            firstSelectedCard = card
-        case 2:
-            secondSelectedCard = card
-        default:
-            cardsSelected = 0
-        }
         
-        if cardsSelected == 2
+        print("Hit playCard")
+        if let selectedCard = sender.view?.tag
         {
-            if firstSelectedCard.image == secondSelectedCard.image
+            if cardsSelected == 0
             {
+                //INFO: Store first selection.
+                firstSelectedCard = cardsImages[selectedCard]
                 
+                //INFO: Advance to next card.
+                cardsSelected += 1
+                
+                flipCard(sender: cardsPhone[selectedCard])
             }
-            else
+            else if cardsSelected == 1
             {
+                //INFO: Store second selection.
+                secondSelectedCard = cardsImages[selectedCard]
                 
+                //INFO: Check for match condition.
+                if firstSelectedCard == secondSelectedCard
+                {
+                    print("Match")
+                }
+                else{
+                    print("No match")
+                }
+                
+                //INFO: Reset to a different attempt.
+                cardsSelected = 0
+                firstSelectedCard = UIImage()
+                secondSelectedCard = UIImage()
+                
+                flipCard(sender: cardsPhone[selectedCard])
             }
         }
     }
     
+    //MARK: - User Options
     
+    @IBAction func cardImageSetChanged(_ sender: UISegmentedControl)
+    {
+        switch sender.selectedSegmentIndex {
+        case 0:
+            imageSet = "r"
+        case 1:
+            imageSet = "k"
+        case 2:
+            imageSet = "m"
+        case 3:
+            imageSet = "p"
+        case 4:
+            imageSet = "g"
+        default:
+            print("This should never happen...")
+        }
+    }
+    
+    //MARK: - Animations
+    func flipCard(sender: UIImageView)
+    {
+        sender.image = UIImage()
+        UIView.transition(with: sender, duration: 0.3, options: .transitionFlipFromLeft, animations: nil, completion: nil)
+    }
 }
 
